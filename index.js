ffedb3bb965fc6e1b5231bf1395697c6e38cc23e
Anita-Mutemi <anita.mutemi@student.moringaschool.com>
2023-06-16 05:30:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(missev, birthday) {
+  let messages = [];
+
+  for (let i = 0; i < missev.length; i++) {
+    let message = `Thank you, ${missev[i]}, for the wonderful ${birthday} gift!`;
+    messages.push(message);
+  }
+
+  return messages;
+}
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
